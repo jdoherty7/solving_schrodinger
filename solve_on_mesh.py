e9e45790480d32754fa16f5a3ef4246572ae844f
John Doherty <jjdoher2@illinois.edu>
2018-04-27 17:30:14 -0500

    linear basis functions used new matrix added to formula

--- solve_on_mesh.py
+++ solve_on_mesh.py
@@ -11,7 +11,7 @@ from basis import *
 
 
 
-def integral(f, n=3):
+def integral(f, n=6):
     # integral on the triangle (0,0), (0,1), (1,0)
     nodes, weights = trigauss(n)
     y = 0
@@ -33,61 +33,80 @@ lx, ly = 1.0, 1.0
 # degree = 1 num_bases = 3
 # degree = 2 num_bases = 6
 num_bases = 3
+elem = "linear" if num_bases == 3 else "quadratic"
 ind = 2 if num_bases == 3 else 9
-E = mesh.Elmts[2][1]
+E = mesh.Elmts[ind][1]
 V = mesh.Verts[:,:2]
 
 ne = E.shape[0] #
 nv = V.shape[0]
-X = V[:,0].astype(np.complex128)      # X shape: nv x 1
-Y = V[:,1].astype(np.complex128)
+X = V[:,0]      # X shape: nv x 1
+Y = V[:,1]
 
 
 def uex(x, y, t=0):
-    nx, ny = 3, 4
-    En = (((hbar*np.pi)**2)/(2*m*lx*ly))*(nx**2 + ny**2)
-    phit = np.exp(-1j*t*En/hbar)
-    N = (np.sqrt(2)/lx)*(np.sqrt(2)/ly)
-    return np.sin(nx*x*np.pi)*np.sin(ny*y*np.pi)*phit
-
-
+    nxs = [1, 2, 2]
+    nys = [1, 1, 2]
+    phis = []
+    for nx, ny in zip(nxs, nys):
+        En = (((hbar*np.pi)**2)/(2*m*lx*ly))*(nx**2 + ny**2)
+        phit = np.exp(-1j*t*En/hbar)
+        N = (np.sqrt(2)/lx)*(np.sqrt(2)/ly)
+        phi = np.sin(nx*x*np.pi)*np.sin(ny*y*np.pi)*phit
+        phis.append(phi)
+    return sum(phis).astype(np.complex128)
 
 
 # Create the LHS and RHS
 AA = np.zeros((ne, num_bases**2), dtype= np.complex128)
 
-IA = np.zeros((ne, num_bases**2), dtype= np.complex128)
-JA = np.zeros((ne, num_bases**2), dtype= np.complex128)
+IA = np.zeros((ne, num_bases**2))
+JA = np.zeros((ne, num_bases**2))
 
 for t in range(ne):
     K = E[t]
     def Jacobian(r, s):
-        dphi = np.zeros((2, num_bases), dtype= np.complex128)
-        J = np.zeros((2,2), dtype= np.complex128)
+        dphi = np.zeros((2, num_bases))
+        J = np.zeros((2,2))
         for i in range(1, num_bases+1):
-            dphi[0, i-1] = dphi_dr(i, r, s)
-            dphi[1, i-1] = dphi_ds(i, r, s)
-        dx = np.dot(dphi, X[K])
-        dy = np.dot(dphi, Y[K])
+            dphi[0, i-1] = dphi_dr(i, r, s, elem=elem)
+            dphi[1, i-1] = dphi_ds(i, r, s, elem=elem)
+        dx = np.dot(dphi, X[K[:num_bases]])
+        dy = np.dot(dphi, Y[K[:num_bases]])
         J[0, :] = dx
         J[1, :] = dy
         return J
 
-
+    # matrix corresponding to the spatial derivatives
     def g(r, s):
-        dphi = np.zeros((2, num_bases), dtype= np.complex128)
+        dphi = np.zeros((2, num_bases))
         J = Jacobian(r, s)
         invJ = la.inv(J.T)
         detJ = la.det(J)
         for i in range(1,num_bases+1):
-            dphi[0, i-1] = dphi_dr(i, r, s)
-            dphi[1, i-1] = dphi_ds(i, r, s)
+            dphi[0, i-1] = dphi_dr(i, r, s, elem=elem)
+            dphi[1, i-1] = dphi_ds(i, r, s, elem=elem)
         dlambda = np.dot(invJ, dphi)
 
-        return detJ*np.dot(dlambda.T, dlambda)
+        return detJ*(dlambda.T @ dlambda)
+
+    # matrix multiplying the derivative
+    def b(r, s):
+        phis = np.zeros((2, num_bases))
+        for i in range(1,num_bases+1):
+            phis[0, i-1] = phi(i, r, s, elem=elem)
+            phis[1, i-1] = phi(i, r, s, elem=elem)
+        return np.dot(phis.T, phis)
 
 
-    A_local = 1j*integral(g)*(hbar/(2.0*m))
+
+    # this is right hand side of 
+    # ih dt u = -h*h/2/m dxx u
+    B_local = integral(b)
+    A_local = integral(g)
+    A_local = np.dot(la.inv(B_local), A_local)
+
+    A_local = 1j*A_local*(hbar/(2.0*m))
 
     # assign to global matrices
     AA[t, :] = A_local.ravel()
@@ -99,7 +118,7 @@ for t in range(ne):
 
 
 
-A = sparse.coo_matrix((AA.ravel(), (IA.ravel(), JA.ravel())), dtype= np.complex128)
+A = sparse.coo_matrix((AA.ravel(), (IA.ravel(), JA.ravel())))
 A = A.tocsr()
 A = A.tocoo()
 
@@ -138,7 +157,7 @@ plot = True
 
 if plot:
     fig = plt.figure()
-nt = 100
+nt = 600
 tf = .01
 # dt may be incorrect
 dt = tf/(nt+1.0)
@@ -147,7 +166,7 @@ errors = {"EB":[], "EF":[]}
 EBM = sparse.eye(A.shape[0]).tocsc() - dt*A.tocsc()
 inv_A = spla.splu(EBM)
 #plt.figure()
-for method in ["EB", "EF"]:
+for method in ["EF"]:
     u = uex(X, Y)
     for i in range(nt):
         ux = uex(X, Y, i*nt)
@@ -156,20 +175,20 @@ for method in ["EB", "EF"]:
         er = la.norm((pdist_true - pdist).flatten(), np.inf)
         errors[method].append(er)
 
-        if plot:
+        if plot and (i%10==0):
             plt.clf()
             ax = fig.add_subplot(1,1,1)
             ax.set_title("Probability Distribution "+ method +" t={0:.3f}/{1:.3f}".format(dt*i, tf))
             ax.set_xlabel("x")
             ax.set_ylabel("y")
             surf = ax.tripcolor(X, Y, pdist, triangles=E[:,:num_bases], cmap=plt.cm.jet, linewidth=0.2)
-            #fig.colorbar(surf)
+            fig.colorbar(surf)
             plt.pause(.001)
 
         # go to next step
         if method == "EF":
             # Euler Forward O(dt)
-            u = u - dt*A.dot(u)
+            u = dt*A.dot(ux)
         else:
             # Euler Backward O(dt) but stable
             u = inv_A.solve(u)
